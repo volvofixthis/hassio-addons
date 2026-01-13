2e5d694dabe841263bd8995ba6b61d2f9ef8196d
Konstantin Losev <losev.konstantin6@rwb.ru>
2026-01-13 12:33:42 +0300

    Added vmalert and alertmanager addons

---
+++ alertmanager/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/with-contenv bashio
+set -xe
+
+# Ensure persistent folder exists
+mkdir -p /data
+
+TEMPLATES_DIR="/data/templates"
+LISTEN_ADDRESS=$(bashio::config 'listen_address')
+CONFIG_FILE=$(bashio::config 'config_file')
+EXTRA_ARGS=$(bashio::config 'extra_args')
+
+# Seed default config on first run (do not overwrite user changes)
+if [ ! -f "${CONFIG_FILE}" ]; then
+  cp /defaults/alertmanager.yml "${CONFIG_FILE}"
+fi
+
+if [ ! -d "${TEMPLATES_DIR}" ]; then
+  cp -R /defaults/templates "${TEMPLATES_DIR}"
+fi
+
+exec /bin/alertmanager \
+  --web.listen-address="${LISTEN_ADDRESS}" \
+  --config.file="${CONFIG_FILE}" \
+  --storage.path=/data/storage \
+  ${EXTRA_ARGS}
+
---
+++ vmalert/run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bashio
+
+set +e  # Allow command errors without killing the addon
+
+NOTIFIER_ENDPOINT=$(bashio::config 'notifier_endpoint')
+VICTORIAMETRICS_ENDPOINT=$(bashio::config 'victoriametrics_endpoint')
+EXTRA_ARGS=$(bashio::config 'extra_args')
+RULES_DIR="/data/rules"
+
+bashio::log.info "Starting vmalert"
+
+if [ ! -d "${TEMPLATES_DIR}" ]; then
+  cp -R /defaults/templates "${TEMPLATES_DIR}"
+fi
+
+/usr/local/bin/vmalert --datasource.url=${VICTORIAMETRICS_ENDPOINT} \
+    --remoteRead.url=${VICTORIAMETRICS_ENDPOINT} \
+    --remoteWrite.url=${VICTORIAMETRICS_ENDPOINT} \
+    --notifier.url=${NOTIFIER_ENDPOINT} \
+    ---rule=/data/rules/*.yml \
+    ${EXTRA_ARGS}
